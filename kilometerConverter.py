baac1534483fd86e79464c14d9f472e85fd318bd
philip72 <philipwamai85@gmail.com>
2022-03-17 21:58:43 +0300

    data types and print

---
+++ kilometerConverter.py
@@ -0,0 +1,5 @@
+kilometers=float(input("enter what u want to convert to miles"))
+kilometersToMiles=0.621371
+miles=kilometersToMiles*kilometersToMiles
+print('%0.2f kilometres is equal to %0.2f miles'%(kilometers,miles))
+
