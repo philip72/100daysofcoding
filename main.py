3832bdbd20c459c3264c0682cf17ec496a24e0c7
philip72 <philipwamai85@gmail.com>
2022-03-17 21:57:05 +0300

    seconf commit variable and data types

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Taking kilometers input from the user
+kilometers = float(input("Enter value in kilometers: "))
+
+# conversion factor
+conv_fac = 0.621371
+
+# calculate miles
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
