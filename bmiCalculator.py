b4271632061d24c39b19f3737296242d9b3d6e62
philip72 <philipwamai85@gmail.com>
2022-03-19 17:55:25 +0300

    functions

--- bmiCalculator.py
+++ bmiCalculator.py
@@ -1,11 +1,20 @@
-weight= int(input("enter your weight"))
-height = int(input("enter your height in meters "))
+# weight = int(input("enter your weight"))
+# height = int(input("enter your height in meters "))
+#
+# bmi = weight / (height ** 2)
+# print(bmi)
+# if bmi < 25:
+#     print("you are okay")
+# elif bmi ==25:
+#     print("need to work out abit")
+# else:
+#     print("you are overweight")
 
-bmi=  weight / (height ** 2)
-print(bmi)
-if bmi <25:
-    print("you are okay")
-elif bmi ==25:
-    print("need to work out abit")
-else:
-    print("you are overweight")
+def bmi_calc(weight, height):
+    weight=int(input('enter your weight'))
+    height=int(input('enter your height'))
+    bmi = weight / (height**2)
+    print(bmi)
+
+
+bmi_calc(weight=0, height= 0)
--- kilometerConverter.py
+++ kilometerConverter.py
@@ -1,5 +1,12 @@
-kilometers=float(input("enter what u want to convert to miles"))
-kilometersToMiles=0.621371
-miles=kilometersToMiles*kilometersToMiles
-print('%0.2f kilometres is equal to %0.2f miles'%(kilometers,miles))
+# kilometers=float(input("enter what u want to convert to miles"))
+# kilometersToMiles=0.621371
+# miles=kilometersToMiles*kilometersToMiles
+# print('%0.2f kilometres is equal to %0.2f miles'%(kilometers,miles))
+#
+def km_convert(miles):
+    kilomet =  float(input('write kilom'))
+    miles= '{:.2f}'.format(0.621371*kilomet)
+    print(miles)
 
+
+km_convert(miles=0)
