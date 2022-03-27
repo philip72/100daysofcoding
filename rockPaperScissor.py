b193b7f85e336ae6a335249a6c22a47305144f17
philip72 <philipwamai85@gmail.com>
2022-03-28 00:08:45 +0300

    rck paper scissors 1

--- rockPaperScissor.py
+++ rockPaperScissor.py
@@ -1,32 +1,34 @@
 import random
 
+
 def the_rules():
-    if player== computer:
+    if player == computer:
         print('you tie')
-    elif player=='r':
-        if computer== 's':
+    elif player == 'r':
+        if computer == 's':
             print('you win ')
         else:
             print('you lose')
     elif player == 'p':
-        if computer=='r':
+        if computer == 'r':
             print('you win')
         else:
             print('you lose')
-    elif player== 's':
-        if computer== 'p':
+    elif player == 's':
+        if computer == 'p':
             print("you win ")
         else:
             print("you lose")
 
 
-
-timesYouWannaPlay= input("how many times do u wanna play")
-player = input("enter your choice ")
-player.lower()
-choices = ["r", 'p', 's']
-computer= random.choice(choices)
-print(f'you chose {player}, computer chose {computer}')
-
-the_rules()
-
+timesYouWannaPlay =int(input("how many times do u wanna play"))
+gameCount = timesYouWannaPlay
+end = 0
+while gameCount != end:
+    player = input("enter your choice ")
+    player.lower()
+    choices = ["r", 'p', 's']
+    computer = random.choice(choices)
+    print(f'you chose {player}, computer chose {computer}')
+    the_rules()
+    gameCount -=1
