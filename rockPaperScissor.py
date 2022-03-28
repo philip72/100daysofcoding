ea7ea5d4ad030d17790fe147e252437247270300
philip72 <philipwamai85@gmail.com>
2022-03-28 22:33:08 +0300

    trying to put game count had to make prototype

--- rockPaperScissor.py
+++ rockPaperScissor.py
@@ -1,29 +1,40 @@
 import random
-
+playerspoints=0
+computerspoints=0
 
 def the_rules():
+    computerPoints= computerspoints
+    userPoint=playerspoints
     if player == computer:
         print('you tie')
     elif player == 'r':
         if computer == 's':
             print('you win ')
+            userPoint+=1
         else:
             print('you lose')
+            computerPoints+=1
     elif player == 'p':
         if computer == 'r':
             print('you win')
+            userPoint+=1
         else:
             print('you lose')
+            computerPoints+=1
     elif player == 's':
         if computer == 'p':
             print("you win ")
+            userPoint+=1
         else:
             print("you lose")
+            computerPoints+=1
+
+    print(f'the computer has{computerPoints},while u have {userPoint}')
 
 
-
-gameCount  =int(input("how many times do u wanna play"))
+gameCount = int(input("how many times do u wanna play"))
 end = 0
+
 while gameCount != end:
     player = input("enter your choice ")
     player.lower()
@@ -32,3 +43,4 @@ while gameCount != end:
     print(f'you chose {player}, computer chose {computer}')
     the_rules()
     gameCount -=1
+
